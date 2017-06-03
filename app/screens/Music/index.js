549f32e3718e9dc31a6ad45463f058e5fe7e0a48
david nakayama <david.m.nakayama@vanderbilt.edu>
2017-06-03 15:57:13 -0700

    Update music view.

--- app/screens/Music/index.js
+++ app/screens/Music/index.js
@@ -1,119 +1,152 @@
 import React, { Component } from 'react';
-import { Actions } from 'react-native-router-flux';
-import { connect } from 'react-redux';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TouchableHighlight,
-    ScrollView,
+  View,
 } from 'react-native';
+import Realm from 'realm';
 
-import LinearGradient from 'react-native-linear-gradient';
-import { colors, gradients } from '../../styles';
-import MusicList from '../../components/Music/index.js';
-import Player from '../../components/Music/Player';
-import { getUser } from '../../actions/accountActions';
-import { fetchRecordings, initializePlayer, uploadSong } from '../../actions/recordingActions';
+import { Recordings } from '../../components/Shared';
+import Schemas from '../../realmSchemas';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+import { DisplayTime, MapRecordingFromDB } from '../../mappers/recordingMapper';
+// import styles from './styles.js';
 
-const mapStateToProps = (state) => ({
-    showPlayer: state.profileReducer.showPlayer, 
-    profile: state.userReducer.profile,
-    user: state.userReducer.user,
-    recordings: mapRealmResults(state.recordingsReducer.recordings),
-    currentRecording: state.recordingsReducer.currentRecording,
-});
+const realm = new Realm(Schemas.RecordingSchema);
 
-const mapDispatchToProps = (dispatch) => ({
-    navigateback: () => {
-        Actions.pop();
-    },
-    getCurrentUser: (user) => { dispatch(getUser(user)); },
-    getRecordings: () => dispatch(fetchRecordings()),
-    initializePlayer: (currentRecording, audio) => dispatch(initializePlayer(currentRecording, audio)),
-    syncRecording: (recording, user) => dispatch(uploadSong(recording, user)), 
-});
+export default class Music extends Component {
+  state = {
+    recordings: realm.objects('Recording').sorted('id', true),
+  };
 
-class Music extends Component {    
-    componentDidMount() {
-        const { getRecordings } = this.props;
-        getRecordings();
-    }
-    
-    updateView = (view) => {
-        // do nothing
-    }
-    
-    render() {
-        const { user, showPlayer, recordings, initializePlayer, syncRecording } = this.props;
-        return(
-            <View style={styles.container}>
-                <MusicNavBar updateView={this.updateView} navigate={this.props.navigateback} />
-                <MusicList recordings={recordings || {}} initializePlayer={initializePlayer} syncRecording={(recording) => { syncRecording(recording, user); }} />
-                { showPlayer ? <Player /> : null }
-            </View>
-        );
-    }
-};
+  componentDidMount() {
+    this._recordings = realm.objects('Recording').sorted('id', true);
+    realm.addListener('change', this.recordingsChange);
+  }
 
-const MusicNavBar = ({navigate, updateView}) => (
-    <LinearGradient colors={gradients.redToOrange} start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={styles.gradient}>
-        <View style={styles.navBar}> 
-            <TouchableHighlight style={styles.navBackArrow} onPress={() => { navigate(); }}>
-                <Image source={require('../../img/back_arrow.png')} style={styles.navBackArrow}/>
-            </TouchableHighlight>
-            <TouchableHighlight onPress={() => { updateView('mymusic'); }} style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
-                <Text style={styles.navTitle}>Recordings</Text>
-            </TouchableHighlight>
-            <View style={{ width: 30, marginRight: 10 }} />
-        </View>
-    </LinearGradient>
-);
+  recordingsChange = () => this.setState({
+    recordings: [...this._recordings.map(x => MapRecordingFromDB(x))],
+  });
 
-const mapRealmResults = (results) => {
-    return results.reduce((map, file) => {
-        map[file.name] = file;
-        return map;
-    }, {});
+  render() {
+    return (
+      <View>
+        <Recordings recordings={this.state.recordings} />
+      </View>
+    );
+  }
 }
+// import { Actions } from 'react-native-router-flux';
+// import { connect } from 'react-redux';
+// import {
+//     StyleSheet,
+//     Text,
+//     View,
+//     Image,
+//     TouchableHighlight,
+//     ScrollView,
+// } from 'react-native';
 
-export default connect(mapStateToProps, mapDispatchToProps)(Music);
+// import LinearGradient from 'react-native-linear-gradient';
+// import { colors, gradients } from '../../styles';
+// import MusicList from '../../components/Music/index.js';
+// import Player from '../../components/Music/Player';
+// import { getUser } from '../../actions/accountActions';
+// import { fetchRecordings, initializePlayer, uploadSong } from '../../actions/recordingActions';
 
-const styles = {
-    container: {
-        backgroundColor: 'white',
-        flex: 1,
-    },
-    navBar: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        height: 45,
-    },
-    navTitle: {
-        padding: 10,
-        color: 'white',
-        fontSize: 20,
-        marginHorizontal: 10,
-    },
-    navBackArrow: {
-        height: 30,
-        width: 30,
-        marginLeft: 10,
-    },
-    green: {
-        backgroundColor: '#31CB94'
-    },
-    gradient: {
-        height: 45,
-        top: 0,
-        right: 0,
-        left: 0,
-        backgroundColor: 'transparent',
-        alignSelf: 'stretch',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-}
+// const mapStateToProps = state => ({
+//   showPlayer: state.profileReducer.showPlayer,
+//   profile: state.userReducer.profile,
+//   user: state.userReducer.user,
+//   recordings: mapRealmResults(state.recordingsReducer.recordings),
+//   currentRecording: state.recordingsReducer.currentRecording,
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   navigateback: () => {
+//     Actions.pop();
+//   },
+//   getCurrentUser: (user) => { dispatch(getUser(user)); },
+//   getRecordings: () => dispatch(fetchRecordings()),
+//   initializePlayer: (currentRecording, audio) => dispatch(initializePlayer(currentRecording, audio)),
+//   syncRecording: (recording, user) => dispatch(uploadSong(recording, user)),
+// });
+
+// class Music extends Component {
+//   componentDidMount() {
+//     const { getRecordings } = this.props;
+//     getRecordings();
+//   }
+
+//   updateView = (view) => {
+//         // do nothing
+//   }
+
+//   render() {
+//     const { user, showPlayer, recordings, initializePlayer, syncRecording } = this.props;
+//     return (
+//       <View style={styles.container}>
+//         <MusicNavBar updateView={this.updateView} navigate={this.props.navigateback} />
+//         <MusicList recordings={recordings || {}} initializePlayer={initializePlayer} syncRecording={(recording) => { syncRecording(recording, user); }} />
+//         { showPlayer ? <Player /> : null }
+//       </View>
+//     );
+//   }
+// }
+
+// const MusicNavBar = ({ navigate, updateView }) => (
+//   <LinearGradient colors={gradients.redToOrange} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.gradient}>
+//     <View style={styles.navBar}>
+//       <TouchableHighlight style={styles.navBackArrow} onPress={() => { navigate(); }}>
+//         <Image source={require('../../img/back_arrow.png')} style={styles.navBackArrow} />
+//       </TouchableHighlight>
+//       <TouchableHighlight onPress={() => { updateView('mymusic'); }} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+//         <Text style={styles.navTitle}>Recordings</Text>
+//       </TouchableHighlight>
+//       <View style={{ width: 30, marginRight: 10 }} />
+//     </View>
+//   </LinearGradient>
+// );
+
+// const mapRealmResults = results => results.reduce((map, file) => {
+//   map[file.name] = file;
+//   return map;
+// }, {});
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Music);
+
+// const styles = {
+//   container: {
+//     backgroundColor: 'white',
+//     flex: 1,
+//   },
+//   navBar: {
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'transparent',
+//     height: 45,
+//   },
+//   navTitle: {
+//     padding: 10,
+//     color: 'white',
+//     fontSize: 20,
+//     marginHorizontal: 10,
+//   },
+//   navBackArrow: {
+//     height: 30,
+//     width: 30,
+//     marginLeft: 10,
+//   },
+//   green: {
+//     backgroundColor: '#31CB94',
+//   },
+//   gradient: {
+//     height: 45,
+//     top: 0,
+//     right: 0,
+//     left: 0,
+//     backgroundColor: 'transparent',
+//     alignSelf: 'stretch',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// };
