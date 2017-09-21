0f0eb7696a6bdbc49e9338451dcd6318c804654c
Ian Mundy <mundycore@gmail.com>
2017-09-20 22:47:29 -0700

    Add ios implementation of metronome

---
+++ ios/noteableMobile/Metronome.h
@@ -0,0 +1,17 @@
+//
+//  Metronome.h
+//  noteableMobile
+//
+//  Created by Ian Mundy on 9/17/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#ifndef Metronome_h
+#define Metronome_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface Metronome : NSObject <RCTBridgeModule>
+@end
+
+#endif /* Metronome_h */
