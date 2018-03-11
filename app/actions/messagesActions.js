a2bb612fb5e6f1f33a8d91aabfd9a62bc74f8ca5
imundy <mundycore@gmail.com>
2018-03-10 21:24:46 -0800

    Add messages actions

---
+++ app/actions/messagesActions.js
@@ -0,0 +1,49 @@
+
+import { MessageActionTypes } from './ActionTypes';
+import { fetchUtil } from '../util';
+
+export const sendMessage = (user, conversationId, content) => async (dispatch) => {
+  const message = await postMessage(user, conversationId, content);
+  dispatch({ type: MessageActionTypes.sendMessage, message });
+};
+
+export const searchNav = () => ({ type: MessageActionTypes.searchMessages });
+
+export const listNav = () => ({ type: MessageActionTypes.listMessages });
+
+export const getConversations = currentUser => async (dispatch) => {
+  const conversations = await fetchConversations(currentUser);
+  const conversationsByUserId = conversations.reduce((map, c) => {
+    const users = [...c.users].reduce((userMap, user) => { userMap[user.id] = user; return userMap; }, {});
+    c.users = users;
+    map[c.id] = c;
+    return map;
+  }, {});
+  dispatch({ type: MessageActionTypes.getConversations, conversations: conversationsByUserId });
+};
+
+export const openConversation = (currentUser, userId, conversationId) => async (dispatch) => {
+  const conversation = await fetchConversation(currentUser, conversationId);
+  const users = conversation.users.reduce((map, user) => { map[user.id] = user; return map; }, {});
+  conversation.users = users;
+  const userName = `${users[userId].firstName} ${users[userId].lastName}`;
+  dispatch({ type: MessageActionTypes.openConversation, conversation, userName });
+};
+
+const fetchConversation = (user, id) => fetchUtil.get({ url: `https://beta.noteable.me/api/v1/conversations/${id}`, auth: user.jwt })
+.then(response => response.json());
+
+const fetchConversations = user => fetchUtil.get({ url: 'https://beta.noteable.me/api/v1/conversations', auth: user.jwt })
+.then(response => response.json());
+
+const postMessage = (user, conversationId, content) => fetchUtil.postWithBody({
+  url: 'https://beta.noteable.me/api/v1/messages',
+  auth: user.jwt,
+  body: {
+    userId: user.id,
+    conversationId,
+    content,
+  },
+})
+.then(response => response.json())
+.then(({ messageId }) => ({ id: messageId, userId: user.id, conversationId, content }));
